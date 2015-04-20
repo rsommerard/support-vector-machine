7f2659c32affc3ee251c671ec432602cb408e39f
Romain SOMMERARD <rsommerard@gmail.com>
2015-04-20 11:59:07 +0200

    Update

--- main.py
+++ main.py
@@ -17,7 +17,10 @@ clf = SVC(C=0.001, kernel='linear')
 clf.fit(X, Y)
 hY = clf.predict(X)
 
+print '-' * 80
+print 'Linear data and linear SVC'
 print 'Erreur:', abs(hY - Y).sum()
+print '-' * 80
 
 for sv in clf.support_vectors_:
   plt.scatter(sv[0], sv[1], color='g')
@@ -27,6 +30,7 @@ w2 = clf.coef_[0][1]
 b = clf.intercept_
 
 print 'w1 = ', w1, '|', 'w2 = ', w2
+print '-' * 80
 
 x11 = 0
 x12 = ((-1 * w1 * x11) / w2) - (b / w2)
@@ -54,16 +58,31 @@ plt.show()
 ################################################################################
 ################################################################################
 
-X, Y = datasets.make_circles(n_samples=400, factor=.3, noise=.05)
+X, Y = datasets.make_circles(n_samples=n_samples, factor=.5, noise=.05)
+Xb = []
+for x in X:
+    Xb.append([1, x[0], x[1], x[0]**2, x[1]**2])
 
-fig, axes = plt.subplots(nrows=1, ncols=1)
-fig.set_size_inches(7, 7)
-axes.scatter(X[:,0], X[:,1], c=Y)
+clf = SVC(kernel='linear')
+clf.fit(Xb, Y)
+hY = clf.predict(Xb)
 
-clf = SVC(C=1, kernel='linear')
+print 'No linear data and linear SVC'
+print 'Erreur:', abs(hY - Y).sum()
+print '-' * 80
+
+################################################################################
+################################################################################
+################################################################################
+
+X, Y = datasets.make_circles(n_samples=n_samples, factor=.5, noise=.05)
+
+clf = SVC(kernel='poly', degree=2)
 clf.fit(X, Y)
 hY = clf.predict(X)
 
+print 'No linear data and poly SVC'
 print 'Erreur:', abs(hY - Y).sum()
+print '-' * 80
 
-plt.show()
+print 'On obtient des erreurs de 0 car on a pas testé par la méthode de cross validation.'
